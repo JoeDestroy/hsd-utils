52645ebc1fddf664820a2f388717ffb0effae94d
JoeDestroy <vladstas@protonmail.com>
2022-02-13 13:30:45 -0500

    Added the get_node_info() function and updated set_credentials()

--- hns_utils.py
+++ hns_utils.py
@@ -1,18 +1,27 @@
 import requests
+import json
 
 network = ""
-node_port = ""
-wallet_port = ""
+node_port = 0
+wallet_port = 0
 api_key = ""
 
 node_request = ""
 wallet_request = ""
 
-def set_credentials(net, api_key, ip):
+def set_credentials(net, apiKey, ip):
+    global network
+    global node_port
+    global wallet_port
+    global api_key
+    
+    global node_request
+    global wallet_request
+    
     network = net
     
     if net == "main":
-        wallet_portort = 12039
+        wallet_port = 12039
     elif net == "testnet":
         wallet_port = 13039
     elif net == "regtest":
@@ -24,12 +33,18 @@ def set_credentials(net, api_key, ip):
         
     node_port = wallet_port - 2;
     
-    api_key = api_key
+    api_key = apiKey
     
     if api_key == None:
-        node_request = f"http://{ip}:{node_port}/"
-        wallet_request = f"http://{ip}:{wallet_port}/"
+        node_request = "http://" + ip + ":" + str(node_port) + "/"
+        wallet_request = "http://" + ip + ":" + str(wallet_port) + "/"
     else:
-        node_request = f"http://x:{api_key}@{ip}:{node_port}/"
-        wallet_request = f"http://x:{api_key}@{ip}:{wallet_port}/"
+        node_request = "http://x:" + api_key + "@" + ip + ":" + str(node_port) + "/"
+        wallet_request = "http://x:" + api_key + "@" + ip + ":" + str(wallet_port) + "/"
 
+
+def get_node_info():
+    
+    ret = requests.get(node_request)
+    
+    return ret.json()

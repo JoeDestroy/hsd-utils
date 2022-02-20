a056ee0d2240f4f6e869713bdc4eb868642b97d2
JoeDestroy <vladstas@protonmail.com>
2022-02-20 17:36:31 -0500

    Created set_credentials_adv() for setting ports, get_node_info(), get_wallets(), create_wallet()

--- hns_utils.py
+++ hns_utils.py
@@ -43,8 +43,46 @@ def set_credentials(net, apiKey, ip):
         wallet_request = "http://x:" + api_key + "@" + ip + ":" + str(wallet_port) + "/"
 
 
+def set_credentials_adv(nodePort, walletPort, apiKey, ip):
+    global network
+    global node_port
+    global wallet_port
+    global api_key
+    
+    global node_request
+    global wallet_request
+    
+    network = net
+    
+    node_port = nodePort
+    wallet_port = walletPort
+    
+    api_key = apiKey
+    
+    if api_key == None:
+        node_request = "http://" + ip + ":" + str(node_port) + "/"
+        wallet_request = "http://" + ip + ":" + str(wallet_port) + "/"
+    else:
+        node_request = "http://x:" + api_key + "@" + ip + ":" + str(node_port) + "/"
+        wallet_request = "http://x:" + api_key + "@" + ip + ":" + str(wallet_port) + "/"
+
+
 def get_node_info():
     
     ret = requests.get(node_request)
     
     return ret.json()
+
+
+def get_wallets():
+    
+    ret = requests.get(wallet_request + "wallet/")
+    
+    return ret.json()
+    
+
+def create_wallet(id, password):
+    
+    header = [{"passphrase": str(password)}]
+    
+    req = requests.put(wallet_request + "wallet/" + id, data=json.dumps(header))
